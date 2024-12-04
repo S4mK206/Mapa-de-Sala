82164475695b30f037de200ff191ec1c46d29735
Samuel Henricky <86206263+S4mK206@users.noreply.github.com>
2024-12-04 11:58:24 -0300

    Editar logica de editar curso

--- App/controller/curso.py
+++ App/controller/curso.py
@@ -19,7 +19,9 @@ def deletarCurso(idCurso):
         return True
     return False
  
-def atualizarCurso(idCurso, idArea, nome, oferta, periodo, cargaHoraria, horasDia, qtdAlunos):
-    if Curso.atualizar(idCurso, idArea, nome, oferta, periodo, cargaHoraria, horasDia, qtdAlunos):
-        return True
+def atualizarCurso(idCurso, dados):
+    if validarInputs(dados) and idCurso:
+        cursoModel = Curso(dados[1], dados[2], dados[3], dados[4], dados[5], dados[6])
+        if cursoModel.atualizar(idCurso, dados):
+            return True
     return False
--- App/view/editarCurso.py
+++ App/view/editarCurso.py
@@ -1,9 +1,52 @@
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import QTimer, pyqtSlot
+from App.controller.curso import listarCursos
+from App.controller.curso import atualizarCurso
+from App.controller.utils import validarAcao
 from PyQt5.uic import loadUi
 
 class EditarCurso(QWidget):
     def __init__(self):
         super().__init__()
         loadUi('App/view/ui/editarCurso.ui',self)
+        self.dicionarioDeAreas = listarCursos()
+        self.popularJanela()
+
+    def popularJanela(self):
+        self.comboxArea()
+
+    @pyqtSlot()
+    def on_btnEditarCurso_clicked(self):
+        print(self.dicionarioDeAreas)
+        info = self.getEditarCurso()
+        idCurso = self.dicionarioDeAreas[info[0]]
+        if atualizarCurso(idCurso, info):
+            validarAcao()
+
+    def comboxArea(self):
+        areas = self.dicionarioDeAreas.keys()
+        self.campoArea.addItems(areas)
+
+    def getEditarCurso(self):
+        area = self.campoArea.currentText().strip()
+        nome = self.nomeCurso.text().strip()
+        oferta = self.ofertaCurso.text().strip()
+        periodo = self.periodoCurso.currentText().strip()
+        carga = self.cargaCurso.text().strip()
+        horas = self.horasPorDia.text().strip()
+        alunos = self.quantidadeAlunos.text().strip()
         
-        # btn editar curso = btnEditarCurso
+        return(area, nome, oferta, periodo, carga, horas, alunos)
+    
+    def validandoDados(self):
+        self.respostas.setText('EDITANDO...')
+        QTimer.singleShot(2000, lambda: self.limparCampos(self.respostas))
+
+    def dadosInvalidos(self):
+        texto = 'DADOS INCOMPLETOS.'
+        self.respostas.setText(texto)
+        QTimer.singleShot(2000, lambda: self.limparCampos(self.respostas))
+
+    def limparCampos(self, campo):
+        campo.clear()
+        
