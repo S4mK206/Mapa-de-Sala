6f0d662f591564124772fb50011de75c47036fd6
Samuel Henricky <86206263+S4mK206@users.noreply.github.com>
2024-12-05 10:01:19 -0300

    tentando popular campos
    Consegui desenvolver a lógica para popular o campo de área, mas não consegui o de curso.

--- App/view/editarCurso.py
+++ App/view/editarCurso.py
@@ -1,6 +1,9 @@
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtCore import QTimer, pyqtSlot
+
+from App.controller.area import listarAreas
 from App.controller.curso import listarCursos
+
 from App.controller.curso import atualizarCurso
 from App.controller.utils import validarAcao
 from PyQt5.uic import loadUi
@@ -9,34 +12,43 @@ class EditarCurso(QWidget):
     def __init__(self):
         super().__init__()
         loadUi('App/view/ui/editarCurso.ui',self)
-        self.dicionarioDeAreas = listarCursos()
+        self.dicionarioDeAreas = listarAreas()
+        self.dicionarioDeCursos = listarCursos()
         self.popularJanela()
 
     def popularJanela(self):
         self.comboxArea()
+        self.comboxCurso()
 
     @pyqtSlot()
     def on_btnEditarCurso_clicked(self):
-        print(self.dicionarioDeAreas)
         info = self.getEditarCurso()
-        idCurso = self.dicionarioDeAreas[info[0]]
+        idCurso = self.dicionarioDeCursos[info[0]]
         if atualizarCurso(idCurso, info):
             validarAcao()
 
     def comboxArea(self):
         areas = self.dicionarioDeAreas.keys()
         self.campoArea.addItems(areas)
+        print(f"Lista de Areas: {self.dicionarioDeAreas}")
+
+    def comboxCurso(self):
+        cursos = self.dicionarioDeCursos.keys()
+        self.alterarCurso.addItem(cursos)
+        print(f"Lista de Cursos: {self.dicionarioDeCursos}")
 
     def getEditarCurso(self):
-        area = self.campoArea.currentText().strip()
         nome = self.nomeCurso.text().strip()
         oferta = self.ofertaCurso.text().strip()
         periodo = self.periodoCurso.currentText().strip()
         carga = self.cargaCurso.text().strip()
+        # area = self.campoArea.currentText().strip()
         horas = self.horasPorDia.text().strip()
         alunos = self.quantidadeAlunos.text().strip()
+        # curso = self.alterarCurso.currentText().strip()
         
-        return(area, nome, oferta, periodo, carga, horas, alunos)
+        return(nome, oferta, periodo, carga, horas, alunos)
+        # return(nome, oferta, area, periodo, carga, curso, horas, alunos)
     
     def validandoDados(self):
         self.respostas.setText('EDITANDO...')
